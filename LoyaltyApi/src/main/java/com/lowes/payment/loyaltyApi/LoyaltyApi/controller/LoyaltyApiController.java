199747d86a8dc43a0a6e10bbcee8275133732d18
Karun-Manjunath <48211110+Karun-Manjunath@users.noreply.github.com>
2019-11-13 20:23:15 +0530

    Added new endpoint to get Loyalty details by passing customerID

--- LoyaltyApi/src/main/java/com/lowes/payment/loyaltyApi/LoyaltyApi/controller/LoyaltyApiController.java
+++ LoyaltyApi/src/main/java/com/lowes/payment/loyaltyApi/LoyaltyApi/controller/LoyaltyApiController.java
@@ -3,23 +3,32 @@ package com.lowes.payment.loyaltyApi.LoyaltyApi.controller;
 import com.lowes.payment.loyaltyApi.LoyaltyApi.domain.dto.LoyaltyDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 
 @RestController
 public class LoyaltyApiController {
     private final static Logger Log = LoggerFactory.getLogger(LoyaltyDto.class);
 
+    HashMap<String,LoyaltyDto> loyaltyList = new HashMap<String,LoyaltyDto>();
+
     @RequestMapping(value = "/loyalty", method = RequestMethod.POST)
     public String saveLoyalty(@RequestBody @Valid LoyaltyDto requestLoyaltyDto){
         requestLoyaltyDto.setLoyaltyId("47XXXXXXXXX0001");
+
+        loyaltyList.put(requestLoyaltyDto.customerId, requestLoyaltyDto);
         Log.info("Customer Id:"+requestLoyaltyDto.customerId + "has been registered with Loyalty Id: "
                 +requestLoyaltyDto.loyaltyId);
+
         return ("Success");
     }
 
+    @ResponseBody
+    @RequestMapping(value="/loyalty/{customerID}", method = RequestMethod.GET)
+    public LoyaltyDto getLoyalty(@PathVariable("customerID")  String customerID){
+
+        return(loyaltyList.get(customerID));
+    }
 }
